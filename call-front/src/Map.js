017b631bc910d4ef2ed31ad3171eb7b19ef723cb
woojinhong03 <hongwoojin471@gmail.com>
2021-05-02 20:21:47 +0900

    Update Map.js

--- call-front/src/Map.js
+++ call-front/src/Map.js
@@ -16,12 +16,26 @@ class Map extends React.Component{
         kakao.maps.load(() => {
           let container = document.getElementById("map");
           let options = {
-            center: new kakao.maps.LatLng(37.506502, 127.053617),
+            center: new kakao.maps.LatLng(37.62759, 126.92315),
             level: 5
           };
   
           const map = new window.kakao.maps.Map(container, options);
-       
+
+          //마커가 표시 될 위치
+          let markerPosition = new kakao.maps.LatLng(
+            37.62759,
+            126.92315
+          );
+
+          // 마커를 생성
+          let marker = new kakao.maps.Marker({
+            position: markerPosition,
+          });
+
+          // 마커를 지도 위에 표시
+          marker.setMap(map);
+          
         });
       };
     }
