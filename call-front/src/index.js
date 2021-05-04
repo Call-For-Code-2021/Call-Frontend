83a253b92fef98767c313c1606a16c8e44512df6
KMBM0916.biz <kmbm0916.biz@gmail.com>
2021-05-04 23:22:06 +0900

    Update setup

--- call-front/src/index.js
+++ call-front/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Map from './Map.js';
-//import Map1 from './Map1.js';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
+import "./index.css"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Map/>
-      {/*<Map1/>*/}
-  </React.StrictMode>,
+    <App/>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
