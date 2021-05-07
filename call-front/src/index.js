f74af2404f0d7e6a4c974b60600eae385a51ef58
woojinhong03 <hongwoojin471@gmail.com>
2021-05-07 09:07:59 +0900

    kakao set up

--- call-front/src/index.js
+++ call-front/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js'
+import SearchPlace from './SearchPlace.js'
 
 ReactDOM.render(
-    <App/>,
+    <SearchPlace/>,
   document.getElementById('root')
 );
 
