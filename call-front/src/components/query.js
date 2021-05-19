b3b8328eeeedc5807bd8d708c9ae4699dfc6c600
KMBM0916.biz <kmbm0916.biz@gmail.com>
2021-05-19 16:43:16 +0900

    query page setup

--- call-front/src/components/query.js
+++ call-front/src/components/query.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import styled from "styled-components";
+import "./query.css";
 
 function query(){
     return (
-        <div className="query">
-            <h1>Query</h1>
-        </div>
+        <QueryArea>
+            <div className="query">
+                <h1>QueryPage</h1>
+            </div>
+        </QueryArea>
     );
 
 }
+
+const QueryArea = styled.div`
+width: auto;
+height: 94vh;
+`;
 
 export default query;
