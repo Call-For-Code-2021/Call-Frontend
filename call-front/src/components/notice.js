b650a6f578d93549d016e219c5d84a9680e55ede
KMBM0916.biz <kmbm0916.biz@gmail.com>
2021-05-19 15:06:43 +0900

    notice.js article Added

--- call-front/src/components/notice.js
+++ call-front/src/components/notice.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './notice.css';
+import styled from "styled-components";
+
+import ArtImg from "./img/art_img.png";
 
 function notice(){
     return (
-        <div className="notice">
-            <h1>ECO-NOTICE</h1>
-        </div>
+        <NoticeArea>
+            <div className="notice">
+                <h3>오늘, <span>친환경</span> 소식</h3>
+
+                <article>
+                    <div className="article_header">
+                        <img src={ArtImg} alt="Article_Img"></img>
+                    </div>
+                </article>
+                <article>
+
+                </article>
+            </div>
+        </NoticeArea>
     );
 
 }
+
+const NoticeArea = styled.div`
+width: auto;
+height: 94vh;
+`;
 
 export default notice;
