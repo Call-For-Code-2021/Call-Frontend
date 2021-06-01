337b6b452492c78c05b3c1b82d35a6f54317f339
kmbm0916 <kmbm0916.biz@gmail.com>
2021-06-01 14:05:37 +0900

    제작중
    아마 한번 갈아 엎을 예정

--- call-front/src/components/query.js
+++ call-front/src/components/query.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,7 +6,80 @@ import "./query.css";
 
 import XImg from "./img/free-icon-letter-x-109602.png";
 
-function query() {
+const { kakao } = window
+
+const query = ({ searchPlace }) => {
+
+    const [Places, setPlaces] = useState([])
+
+    useEffect(() => {
+        var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
+        const mapContainer = document.getElementById('map')
+        const mapOption = {
+            center: new kakao.maps.LatLng(37.57813143829081, 126.9773018596565), //지도의 중심좌표
+            level: 5,
+        }
+        const map = new kakao.maps.Map(mapContainer, mapOption)  // 지도 생성 및 객체 리턴
+
+        const ps = new kakao.maps.services.Places()
+
+        ps.keywordSearch(placesSearchCB)
+
+        function placesSearchCB(data, status, pagination) {
+            if (status === kakao.maps.services.Status.OK) {
+                let bounds = new kakao.maps.LatLngBounds()
+
+                map.setBounds(bounds)
+                // 페이지 목록 보여주는 displayPagination() 추가
+                displayPagination(pagination)
+                setPlaces(data)
+            }
+
+            else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+                alert('검색 결과가 존재하지 않습니다.\nSearch results do not exist.');
+                return;
+            }
+
+            else if (status === kakao.maps.services.Status.ERROR) {
+                alert('검색 중 오류가 발생했습니다.\nAn error occurred while searching.');
+                return;
+            }
+
+        }
+
+
+        // 검색결과 목록 하단에 페이지 번호 표시
+        function displayPagination(pagination) {
+            var paginationEl = document.getElementById('pagination'),
+                fragment = document.createDocumentFragment(),
+                i
+
+            // 기존에 추가된 페이지 번호 삭제
+            while (paginationEl.hasChildNodes()) {
+                paginationEl.removeChild(paginationEl.lastChild)
+            }
+
+            for (i = 1; i <= pagination.last; i++) {
+                var el = document.createElement('a')
+                el.href = '#'
+                el.innerHTML = i
+
+                if (i === pagination.current) {
+                    el.className = 'on'
+                } else {
+                    el.onclick = (function (i) {
+                        return function () {
+                            pagination.gotoPage(i)
+                        }
+                    })(i)
+                }
+
+                fragment.appendChild(el)
+            }
+            paginationEl.appendChild(fragment)
+        }
+    }, [searchPlace])
+
     return (
         <QueryArea>
             <div className="query">
@@ -19,18 +92,42 @@ function query() {
                     />
                     <button className="bani btn-outline-success" type="submit">검색</button>
                 </form>
-                <div className="article_header">
-                    <h4>최근 검색어</h4>
+                <div className="recent_query">
+                    <div className="article_header">
+                        <h4>최근 검색어</h4>
+                    </div>
+                    <div className="article_body">
+                        <div className="article_body_con">
+                            <p>서울 종로구 대학로</p>
+                            <img src={XImg} alt="X_btn"/>
+                        </div>
+                        <div className="article_body_con">
+                            <p>서울 종로구 대학로</p>
+                            <img src={XImg} alt="X_btn"/>
+                        </div>
+                    </div>
                 </div>
-                <div className="article_body">
-                    <div className="article_body_con">
-                        <p>서울 종로구 대학로</p>
-                        <img src={XImg} alt="X_btn"/>
-                    </div>
-                    <div className="article_body_con">
-                        <p>서울 종로구 대학로</p>
-                        <img src={XImg} alt="X_btn"/>
-                    </div>
+            </div>
+            <div className="result_query">
+                <div id="result-list">
+                    {Places.map((item, i) => (
+                        <div key={i} style={{ marginTop: '20px'}}>
+                            <span>{i + 1}</span>
+                            <div>
+                                <h5>{item.place_name}</h5>
+                                {item.road_address_name ? (
+                                    <div>
+                                        <span>{item.road_address_name}</span>
+                                        <span>{item.address_name}</span>
+                                    </div>
+                                ) : (
+                                    <span>{item.address_name}</span>
+                                )}
+                                <span>{item.phone}</span>
+                            </div>
+                        </div>
+                    ))}
+                    <div id="pagination"></div>
                 </div>
             </div>
         </QueryArea>
